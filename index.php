38d60160842d6cc3aba03688cecc66a354928b00
unknown <david>
2020-01-14 17:26:01 -0300

    select com login e senha previsto

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
     require_once("config.php");
-    $root = new Usuario();
-    $root->loadbyId(3);
-    echo $root;
+    
+    //Carrega um usuario
+    //$root = new Usuario();
+    //$root->loadbyId(3);
+    //echo $root;
+
+    //Carrega uma lista de usuarios
+    //$lista = Usuario::getList();
+    //echo json_encode($lista);
+
+    //Carrega uma lista de usuários buscando pelo login
+    //$search = Usuario::search("jo");
+    //echo json_encode($search);
+
+    //Carrega um usuario usando o login e a senha
+    $usuario = new Usuario();
+    $usuario->login("root","!@#%");
+
+    echo $usuario;
 ?>
